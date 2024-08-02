8cfe1d9a27de6e4c8126204ca68bfbd27ac77f5c
DanendraAthallah-uhuy <danendra_indiarto_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:08:53 +0700

    Add files via upload

---
+++ Belajar Coding bu aulia/Hello.java
@@ -0,0 +1,21 @@
+public class Hello{
+    public static void main(String[] args) {
+        
+        String nama= "Danendra Athallah Indiarto";
+        String asalkota= "Kediri";
+        int usia= 15;
+        double beratbadan=45.00;
+        float tinggibadan=160.0f;
+        char jeniskelamin= 'L';
+        boolean status= false;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Asal kota: "+asalkota);
+        System.out.println("Usia: "+usia);
+        System.out.println("Berat badan: "+beratbadan);
+        System.out.println("Tinggi badan: "+tinggibadan);
+        System.out.println("Satus: "+status);
+
+
+    }
+}
